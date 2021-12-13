1869494c9530029dbba76681fed23e7a703f3e05
Euphemia Deathridge <ephdtrg@mintyclouds.in>
2021-12-13 17:57:21 +0300

    remove accounts task

--- gold_crowdsale/accounts/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import BlockchainAccount
-
-
-class BlockchainAccountSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BlockchainAccount
-        fields = ['eth_address', 'btc_address']
--- gold_crowdsale/scheduler/tasks.py
+++ gold_crowdsale/scheduler/tasks.py
@@ -1,34 +1,14 @@
 import sys
 import logging
 import traceback
-import datetime
 import dramatiq
 
 from django.db import transaction, OperationalError
-from django.utils import timezone
 
-from gold_crowdsale.settings import SCHEDULER_SETTINGS, DEFAULT_TIME_FORMAT
-from gold_crowdsale.accounts.models import BlockchainAccount
+from gold_crowdsale.settings import DEFAULT_TIME_FORMAT
 from gold_crowdsale.rates.models import create_rate_obj
 from gold_crowdsale.rates.serializers import UsdRateSerializer
 from gold_crowdsale.transfers.models import TokenTransfer
-
-
-@dramatiq.actor(max_retries=0)
-def check_and_release_accounts():
-    receiving_accounts = BlockchainAccount.objects.filter(status=BlockchainAccount.Status.RECEIVING)
-
-    updated_accounts = 0
-    for account in receiving_accounts:
-        is_timeout_passed = timezone.now() > \
-            account.last_updated + datetime.timedelta(seconds=SCHEDULER_SETTINGS.get('accounts_drop_timeout'))
-
-        if is_timeout_passed:
-            account.set_available()
-            updated_accounts += 1
-
-    task_time = timezone.now().strftime(DEFAULT_TIME_FORMAT)
-    logging.info(f'ACCOUNTS TASK: Receiving status dropped for {updated_accounts} accounts successfully at {task_time}')
 
 
 @dramatiq.actor(max_retries=0)
