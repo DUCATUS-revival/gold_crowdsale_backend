31a4d29947f4e4e2c3e9e866b3ddccf98e2c235e
Euphemia Deathridge <ephdtrg@mintyclouds.in>
2022-01-14 00:14:06 +0300

    fiat view via api key

--- gold_crowdsale/transfers/views.py
+++ gold_crowdsale/transfers/views.py
@@ -6,12 +6,15 @@ from rest_framework import status
 from rest_framework.views import APIView
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
+from rest_framework_api_key.permissions import HasAPIKey
 
 from .models import create_transfer
 from .serializers import FiatTokenPurchaseSerializer
 
 
 class FiatTransferView(APIView):
+    permission_classes = [HasAPIKey]
+
     @swagger_auto_schema(
         operation_description="post ducx address, amount and api-key to send tokens from fiat",
         request_body=openapi.Schema(
